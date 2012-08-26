78fe86fca1ab404483aa5c28bd0bf2a140031e2e
daniel.dreibrodt@gmail.com <daniel.dreibrodt@gmail.com@08b5d097-3e27-63a3-4c6f-efc316e1e7e5>
2012-08-26 11:25:29 +0000

    [maven-release-plugin]  copy for tag dd-plist-1.0

---
+++ tags/dd-plist-1.0/src/main/java/com/dd/plist/BinaryPropertyListWriter.java
@@ -0,0 +1,210 @@
+/*
+ * plist - An open source library to parse and generate property lists
+ * Copyright (C) 2012 Keith Randall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dd.plist;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A BinaryPropertyListWriter is a helper class for writing out
+ * binary property list files.  It contains an output stream and
+ * various structures for keeping track of which NSObjects have
+ * already been serialized, and where they were put in the file.
+ * @author Keith Randall
+ */
+public class BinaryPropertyListWriter {
+
+    /**
+     * Writes a binary plist file with the given object as the root.
+     * @param file the file to write to
+     * @param root the source of the data to write to the file
+     * @throws IOException
+     */
+    public static void write(File file, NSObject root) throws IOException {
+	OutputStream out = new FileOutputStream(file);
+	write(out, root);
+	out.close();
+    }
+    /**
+     * Writes a binary plist serialization of the given object as the root.
+     * @param out the stream to write to
+     * @param root the source of the data to write to the stream
+     * @throws IOException
+     */
+    public static void write(OutputStream out, NSObject root) throws IOException {
+	BinaryPropertyListWriter w = new BinaryPropertyListWriter(out);
+	w.write(root);
+    }
+
+    /**
+     * Writes a binary plist serialization of the given object as the root
+     * into a byte array.
+     * @param root The root object of the property list
+     * @return The byte array containing the serialized property list
+     * @throws IOException
+     */
+    public static byte[] writeToArray(NSObject root) throws IOException {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        write(bout, root);
+        return bout.toByteArray();
+    }
+    
+    // raw output stream to result file
+    OutputStream out;
+    
+    // # of bytes written so far
+    long count;
+    
+    // map from object to its ID
+    Map<NSObject,Integer> idMap = new HashMap<NSObject,Integer>();
+    int idSizeInBytes;
+    
+    BinaryPropertyListWriter(OutputStream outStr) throws IOException {
+	out = new BufferedOutputStream(outStr);
+    }
+    void write(NSObject root) throws IOException {
+	// magic number
+	write("bplist00".getBytes());
+	
+	// assign IDs to all the objects.
+	root.assignIDs(this);
+	
+	idSizeInBytes = computeIdSizeInBytes(idMap.size());
+	
+	// offsets of each object, indexed by ID
+	long[] offsets = new long[idMap.size()];
+	
+	// write each object, save offset
+	for (Map.Entry<NSObject,Integer> entry : idMap.entrySet()) {
+	    NSObject obj = entry.getKey();
+	    int id = entry.getValue();
+	    offsets[id] = count;
+	    if (obj == null) {
+		write(0x00);
+	    } else {
+		obj.toBinary(this);
+	    }
+	}
+	
+	// write offset table
+	long offsetTableOffset = count;
+	int offsetSizeInBytes = computeOffsetSizeInBytes(count);
+	for (long offset : offsets) {
+	    writeBytes(offset, offsetSizeInBytes);
+	}
+	
+	// write trailer
+	// 6 null bytes
+	write(new byte[6]);
+	// size of an offset
+	write(offsetSizeInBytes);
+	// size of a ref
+	write(idSizeInBytes);
+	// number of objects
+	writeLong(idMap.size());
+	// top object
+	writeLong(idMap.get(root));
+	// offset table offset
+	writeLong(offsetTableOffset);
+	
+	out.flush();
+    }
+    
+    void assignID(NSObject obj) {
+	if (!idMap.containsKey(obj)) {
+	    idMap.put(obj, idMap.size());
+	}
+    }
+    
+    int getID(NSObject obj) {
+	return idMap.get(obj);
+    }
+    
+    private static int computeIdSizeInBytes(int numberOfIds) {
+    	if (numberOfIds < 256) return 1;
+    	if (numberOfIds < 65536) return 2;
+    	return 4;
+    }
+    
+    private int computeOffsetSizeInBytes(long maxOffset) {
+	if (maxOffset < 256) return 1;
+	if (maxOffset < 65536) return 2;
+	if (maxOffset < 4294967296L) return 4;
+	return 8;
+    }
+    
+    void writeIntHeader(int kind, int value) throws IOException {
+	assert value >= 0;
+	if (value < 15) {
+	    write((kind << 4) + value);
+	} else if (value < 256) {
+	    write((kind << 4) + 15);
+	    write(0x10);
+	    writeBytes(value, 1);
+	} else if (value < 65536) {
+	    write((kind << 4) + 15);
+	    write(0x11);
+	    writeBytes(value, 2);
+	} else {
+	    write((kind << 4) + 15);
+	    write(0x12);
+	    writeBytes(value, 4);
+	}
+    }
+    
+    void write(int b) throws IOException {
+	out.write(b);
+	count++;
+    }
+    
+    void write(byte[] bytes) throws IOException {
+	out.write(bytes);
+	count += bytes.length;
+    }
+    
+    void writeBytes(long value, int bytes) throws IOException {
+	// write low-order bytes big-endian style
+	for (int i = bytes - 1; i >= 0; i--) {
+	    write((int)(value >> (8 * i)));
+	}
+    }
+    
+    void writeID(int id) throws IOException {
+	writeBytes(id, idSizeInBytes);
+    }
+    
+    void writeLong(long value) throws IOException {
+	writeBytes(value, 8);
+    }
+    
+    void writeDouble(double value) throws IOException {
+	writeLong(Double.doubleToRawLongBits(value));
+    }
+}
---
+++ tags/dd-plist-1.0/src/main/java/com/dd/plist/NSDictionary.java
@@ -0,0 +1,314 @@
+/*
+ * plist - An open source library to parse and generate property lists
+ * Copyright (C) 2012 Daniel Dreibrodt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dd.plist;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A NSDictionary is a collection of keys and values, essentially a Hashtable.
+ * The keys are simple Strings whereas the values can be any kind of NSObject.
+ * 
+ * You can access the keys through the function <code>allKeys()</code>. Access
+ * to the objects stored for each key is given through the function <code>objectoForKey(String key)</code>.
+ * 
+ * @see java.util.Hashtable
+ * @see com.dd.plist.NSObject
+ * @author Daniel Dreibrodt
+ */
+public class NSDictionary extends NSObject {
+
+    private HashMap<String, NSObject> dict;
+
+    /**
+     * Creates a new empty NSDictionary.
+     */
+    public NSDictionary() {
+        //With a linked HashMap the order of elements in the dictionary is kept.
+        dict = new LinkedHashMap<String, NSObject>();
+    }
+
+    /**
+     * Gets the NSObject stored for the given key.
+     * @param key The key.
+     * @return The object.
+     */
+    public NSObject objectForKey(String key) {
+        return dict.get(key);
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, NSObject obj) {
+        dict.put(key, obj);
+    }
+    
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, String obj) {
+        put(key, new NSString(obj));
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, int obj) {
+        put(key, new NSNumber(obj));
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, long obj) {
+        put(key, new NSNumber(obj));
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, double obj) {
+        put(key, new NSNumber(obj));
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, boolean obj) {
+        put(key, new NSNumber(obj));
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, Date obj) {
+        put(key, new NSDate(obj));
+    }
+
+    /**
+     * Puts a new key-value pair into this dictionary.
+     * @param key The key.
+     * @param obj The value.
+     */
+    public void put(String key, byte[] obj) {
+        put(key, new NSData(obj));
+    }
+
+    /**
+     * Removes a key-value pair from this dictionary.
+     * @param key The key
+     */
+    public void remove(String key){
+        dict.remove(key);
+    }
+
+    /**
+     * Counts the number of contained key-value pairs.
+     * @return The size of this NSDictionary.
+     */
+    public int count() {
+        return dict.size();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj.getClass().equals(this.getClass()) && ((NSDictionary)obj).dict.equals(dict));
+    }
+
+    /**
+     * Gets a list of all keys used in this NSDictionary.
+     * @return The list of all keys used in this NSDictionary.
+     */
+    public String[] allKeys() {
+        return dict.keySet().toArray(new String[0]);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + (this.dict != null ? this.dict.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    void toXML(StringBuilder xml, int level) {
+        indent(xml, level);
+        xml.append("<dict>");
+        xml.append(NSObject.NEWLINE);
+        for (String key:dict.keySet()) {
+            NSObject val = objectForKey(key);
+            indent(xml, level+1);
+            xml.append("<key>");
+            //According to http://www.w3.org/TR/REC-xml/#syntax node values must not
+            //contain the characters < or &. Also the > character should be escaped.
+            if(key.contains("&") || key.contains("<") || key.contains(">")) {
+                xml.append("<![CDATA[");
+                xml.append(key.replaceAll("]]>", "]]]]><![CDATA[>"));
+                xml.append("]]>");
+            }
+            else {
+                xml.append(key);
+            }
+            xml.append("</key>");
+            xml.append(NSObject.NEWLINE);
+            val.toXML(xml, level+1);
+            xml.append(NSObject.NEWLINE);
+        }
+        indent(xml, level);
+        xml.append("</dict>");        
+    }
+
+    @Override
+    void assignIDs(BinaryPropertyListWriter out) {
+	super.assignIDs(out);
+	for (Map.Entry<String,NSObject> entry : dict.entrySet()) {
+	    new NSString(entry.getKey()).assignIDs(out);
+	    entry.getValue().assignIDs(out);
+	}
+    }
+
+    @Override
+    void toBinary(BinaryPropertyListWriter out) throws IOException {
+	out.writeIntHeader(0xD, dict.size());
+	Set<Map.Entry<String,NSObject>> entries = dict.entrySet();
+	for (Map.Entry<String,NSObject> entry : entries) {
+	    out.writeID(out.getID(new NSString(entry.getKey())));
+	}
+	for (Map.Entry<String,NSObject> entry : entries) {
+	    out.writeID(out.getID(entry.getValue()));
+	}
+    }
+    
+    /**
+     * Generates a valid ASCII property list which has this NSDictionary as its
+     * root object. The generated property list complies with the format as
+     * described in <a href="https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/PropertyLists/OldStylePlists/OldStylePLists.html">
+     * Property List Programming Guide - Old-Style ASCII Property Lists</a>.
+     * @return ASCII representation of this object.
+     */
+    public String toASCIIPropertyList() {
+        StringBuilder ascii = new StringBuilder();
+        toASCII(ascii, 0);
+        ascii.append(NEWLINE);
+        return ascii.toString();
+    }
+    
+    /**
+     * Generates a valid ASCII property list in GnuStep format which has this
+     * NSDictionary as its root object. The generated property list complies with
+     * the format as described in <a href="http://www.gnustep.org/resources/documentation/Developer/Base/Reference/NSPropertyList.html">
+     * GnuStep - NSPropertyListSerialization class documentation
+     * </a>
+     * @return GnuStep ASCII representation of this object.
+     */
+    public String toGnuStepASCIIPropertyList() {
+        StringBuilder ascii = new StringBuilder();
+        toASCIIGnuStep(ascii, 0);
+        ascii.append(NEWLINE);
+        return ascii.toString();
+    }
+
+    @Override
+    protected void toASCII(StringBuilder ascii, int level) {
+        indent(ascii, level);
+        ascii.append(ASCIIPropertyListParser.DICTIONARY_BEGIN_TOKEN);
+        ascii.append(NSObject.NEWLINE);
+        String[] keys = dict.keySet().toArray(new String[0]);
+        for (int i=0;i<keys.length;i++) {
+            String key = keys[i];
+            NSObject val = objectForKey(key);
+            indent(ascii, level+1);
+            ascii.append("\"");
+            ascii.append(NSString.escapeStringForASCII(key));
+            ascii.append("\" =");
+            Class<?> objClass = val.getClass();
+            if(objClass.equals(NSDictionary.class) || objClass.equals(NSArray.class) || objClass.equals(NSData.class)) {              
+                ascii.append(NEWLINE);                
+                val.toASCII(ascii, level+2);
+            } else {
+                ascii.append(" ");
+                val.toASCII(ascii, 0);
+            }
+            ascii.append(ASCIIPropertyListParser.DICTIONARY_ITEM_DELIMITER_TOKEN);
+            if(i!=keys.length-1) {
+                ascii.append(NEWLINE);
+            } else if(objClass.equals(NSDictionary.class) || objClass.equals(NSArray.class) || objClass.equals(NSData.class)) {
+                ascii.append(NEWLINE);
+                indent(ascii, level);
+            }
+        }
+        ascii.append(ASCIIPropertyListParser.DICTIONARY_END_TOKEN);
+    }
+
+    @Override
+    protected void toASCIIGnuStep(StringBuilder ascii, int level) {
+        indent(ascii, level);
+        ascii.append(ASCIIPropertyListParser.DICTIONARY_BEGIN_TOKEN);
+        ascii.append(NSObject.NEWLINE);
+        String[] keys = dict.keySet().toArray(new String[0]);
+        for (int i=0;i<keys.length;i++) {
+            String key = keys[i];
+            NSObject val = objectForKey(key);
+            indent(ascii, level+1);
+            ascii.append("\"");
+            ascii.append(NSString.escapeStringForASCII(key));
+            ascii.append("\" =");
+            Class<?> objClass = val.getClass();
+            if(objClass.equals(NSDictionary.class) || objClass.equals(NSArray.class) || objClass.equals(NSData.class)) {              
+                ascii.append(NEWLINE);                
+                val.toASCIIGnuStep(ascii, level+2);
+            } else {
+                ascii.append(" ");
+                val.toASCIIGnuStep(ascii, 0);
+            }
+            ascii.append(ASCIIPropertyListParser.DICTIONARY_ITEM_DELIMITER_TOKEN);
+            if(i!=keys.length-1) {
+                ascii.append(NEWLINE);
+            } else if(objClass.equals(NSDictionary.class) || objClass.equals(NSArray.class) || objClass.equals(NSData.class)) {
+                ascii.append(NEWLINE);
+                indent(ascii, level);
+            }
+        }
+        ascii.append(ASCIIPropertyListParser.DICTIONARY_END_TOKEN);
+    }
+}
