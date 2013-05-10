545fe6ca7c22acbdf170b4ffa049eb6f11c447c5
daniel.dreibrodt@gmail.com <daniel.dreibrodt@gmail.com@08b5d097-3e27-63a3-4c6f-efc316e1e7e5>
2013-05-10 09:00:20 +0000

    Preparing release to maven central repository

--- trunk/src/test/java/com/dd/plist/test/IssueTest.java
+++ trunk/src/test/java/com/dd/plist/test/IssueTest.java
@@ -67,8 +67,7 @@ public class IssueTest extends TestCase {
     }
     public static void testIssue38() throws Exception {
         NSDictionary dict = (NSDictionary)PropertyListParser.parse(new File("test-files/issue33.pbxproj"));
-        NSObject fileRef = (NSObject) ((NSDictionary) ((NSDictionary)dict.get("objects")).get("65541A9C16D13B8C00A968D5")).get("fileRef");
-        System.out.println(fileRef.toXMLPropertyList());
+        NSObject fileRef = ((NSDictionary) ((NSDictionary)dict.get("objects")).get("65541A9C16D13B8C00A968D5")).get("fileRef");
         assertTrue(fileRef.equals(new NSString("65541A9B16D13B8C00A968D5")));
     }
 }
